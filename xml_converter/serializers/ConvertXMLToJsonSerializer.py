fde8252e7a0c29df1d9c11ae6ca5c41a32cec4fa
vu duc tai <tai@inspectorio.com>
2022-02-23 17:03:26 +0700

    Fix converter

--- xml_converter/serializers/ConvertXMLToJsonSerializer.py
+++ xml_converter/serializers/ConvertXMLToJsonSerializer.py
@@ -6,12 +6,32 @@ import json
 class CustomFileField(serializers.FileField):
 
     def to_representation(self, data):
-        return self.convert_file_to_json(data)
-
-    def convert_file_to_json(self, data):
         obj = xmltodict.parse(data)
         data_json = json.loads(json.dumps(obj))
-        return {k: v if v is not None else '' for k, v in data_json.items()}
+        root = data_json and list(data_json.keys())[0]
+        if root:
+            root_data = self._transform(data_json[root]) or ''
+            return {root: root_data}
+
+        return root
+
+    def _transform(self, data):
+        if isinstance(data, dict):
+            res = []
+            for k, v in data.items():
+                if isinstance(v, list):
+                    res.extend([{k: self._transform(item)} for item in v])
+                elif isinstance(v, dict):
+                    res.append({k: self._transform(v)})
+                else:
+                    res.append({k: v if v else ''})
+
+            data = res
+
+        elif isinstance(data, list):
+            data = [self._transform(item) for item in data]
+
+        return data
 
 
 class ConvertXMLToJsonSerializer(serializers.Serializer):
